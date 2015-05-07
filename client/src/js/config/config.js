42fc6645ecb812f9d0d4e782b0180ac7074b6233
James Lewis <jamesl@thoughtworks.com>
2015-05-07 16:09:19 +1000

    update blue team firebase backend

--- client/src/js/config/config.js
+++ client/src/js/config/config.js
@@ -1,9 +1,9 @@
 var Config = {};
 
 if(process.env.NODE_ENV === 'production') {
-  Config.firebaseUrl = 'https://watch-edition-prod.firebaseIO.com';
+  Config.firebaseUrl = 'https://blue-watch-prod.firebaseIO.com';
 } else {
-  Config.firebaseUrl = 'https://watch-edition-dev.firebaseIO.com';
+  Config.firebaseUrl = 'https://blue-watch-dev.firebaseIO.com';
 }
 
 module.exports = Config;
